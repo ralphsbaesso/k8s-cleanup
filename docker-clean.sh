c54be35aa2fcadeb7d634503095a275520928a4d
ralph baesso <WO7Q/xWOa7wTplkZj6l8Gx6j9jqDYsqQX5qXevyHsKE=>
2021-11-24 15:37:07 -0300

    adicionado limpeza de volumes não utilizados no script docker-clean.sh

--- docker-clean.sh
+++ docker-clean.sh
@@ -4,6 +4,7 @@ while true; do
 	
   # https://docs.docker.com/engine/reference/commandline/image_prune/
   docker system prune --all --force
+  docker volume prune --force
 
   # DOCKER_CLEAN_INTERVAL defaults to 30min
   sleep $DOCKER_CLEAN_INTERVAL
